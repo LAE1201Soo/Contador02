5615b3d23eaee265c6d5b92f20630c3f56136354
LAE1201Soo <te200111566@tequila.tecmm.edu.mx>
2023-03-10 16:31:37 -0600

    Initial commit

---
+++ app/src/main/java/com/example/contador/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.contador
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var contador = 0
+    private var incrementar = true
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val lblContador = findViewById<TextView>(R.id.txtContador)
+        val buttonInicio = findViewById<Button>(R.id.btnComenzar)
+        val buttonRefresh = findViewById<Button>(R.id.btnActualizar)
+
+        buttonInicio.setOnClickListener {
+            if (incrementar) {
+                contador++
+                if (contador >= 10) {
+                    incrementar = false
+                }
+            } else {
+                contador--
+                if (contador <= 0) {
+                    incrementar = true
+                }
+            }
+            lblContador.text = contador.toString()
+        }
+
+        buttonRefresh.setOnClickListener {
+            contador = 0
+            incrementar = true
+            lblContador.text = contador.toString()
+        }
+    }
+}
